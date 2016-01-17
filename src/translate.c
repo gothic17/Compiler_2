0cb3bb579a4209451c660fd6d3b010efa200b13e
gothic17 <b.rzepkowski94@gmail.com>
2016-01-17 22:48:39 +0200

    Działa przełożenie na kod trójadresowy IF, IF-ELSE

--- src/translate.c
+++ src/translate.c
@@ -56,6 +56,25 @@ char *generate_new_label() {
 void interpret(node *p) {
 
 	if (strcmp(p->string, ":=") == 0) {
+		if(is_numeric(p->children[0]->string) == 0) {
+			if(search_for_symbol(p->children[0]->string) == 0) {
+				char *string = calloc(strlen("Nie zadeklarowano zmiennej: ") + strlen(p->children[0]->string), sizeof(char));
+				strcat(string, "Nie zadeklarowano zmiennej: ");
+				strcat(string, p->children[0]->string);
+				add_error(create_error(string, p->children[0]->first_line, p->children[0]->first_column,
+						p->children[0]->last_line, p->children[0]->last_column));
+			}
+		}
+		if(is_numeric(p->children[1]->string) == 0) {
+			if(search_for_symbol(p->children[1]->string) == 0) {
+				char *string = calloc(strlen("Nie zadeklarowano zmiennej: ") + strlen(p->children[1]->string), sizeof(char));
+				strcat(string, "Nie zadeklarowano zmiennej: ");
+				strcat(string, p->children[1]->string);
+				add_error(create_error(string, p->children[1]->first_line, p->children[1]->first_column,
+										p->children[1]->last_line, p->children[1]->last_column));
+			}
+		}
+
 		add_four(":=", p->children[0]->string, "", p->children[1]->string, p->children[0]->string);
 	}
 
@@ -439,9 +458,75 @@ void translate(node *p) {
 		}
 	}
 
-	while (i < p->number_of_children) {
-		translate(p->children[i]);
-		i++;
+	else if (strcmp(p->string, "IF") == 0) {
+		// Najpier dodaj warunek z wewnątrz IF
+		interpret(p->children[0]);
+
+		// Utwórz nowy symbol (t2) z przykładu "IFZ t1 GOTO L1 t2"
+		char *symbol_name = generate_new_address();
+		symbol *new_symbol = create_symbol(symbol_name, "-", address_counter, 1,
+				p->first_line, p->first_column, p->last_line, p->last_column);
+		address_counter++;
+		add_symbol(new_symbol);
+
+		// Utwórz etykietę L1 z powyzszego przykładu
+		char *new_label = generate_new_label();
+
+		add_four("IFZ", p->children[0]->string, "GOTO", new_label, symbol_name);
+
+		translate(p->children[1]);
+
+		add_four("", "", "", "", new_label);
+
+		p->string = symbol_name;
 	}
-	interpret(p);
+
+	else if (strcmp(p->string, "IF_ELSE") == 0) {
+		interpret(p->children[0]);
+
+		// Utwórz nowy symbol (t2) z przykładu "IFZ t1 GOTO L1 t2"
+		char *symbol_name_1 = generate_new_address();
+		symbol *new_symbol = create_symbol(symbol_name_1, "-", address_counter, 1,
+				p->first_line, p->first_column, p->last_line, p->last_column);
+		address_counter++;
+		add_symbol(new_symbol);
+
+		// Utwórz etykietę L1 z powyzszego przykładu
+		char *new_label_1 = generate_new_label();
+
+		add_four("IFZ", p->children[0]->string, "GOTO", new_label_1, symbol_name_1);
+
+		p->string = symbol_name_1;
+
+		// Komendy dla prawdziwego IF
+		translate(p->children[1]);
+
+		char *symbol_name_2 = generate_new_address();
+		new_symbol = create_symbol(symbol_name_2, "-", address_counter, 1,
+				p->first_line, p->first_column, p->last_line, p->last_column);
+		address_counter++;
+		add_symbol(new_symbol);
+
+		char *new_label_2 = generate_new_label();
+
+		p->string = symbol_name_2;
+
+		add_four("GOTO", new_label_2, "", "", symbol_name_2);
+
+
+		add_four("","","","", new_label_1);
+
+		// Komendy dla fałszywego IF (za ELSE)
+		translate(p->children[2]);
+
+		add_four("", "", "", "", new_label_2);
+	}
+
+	else {
+		while (i < p->number_of_children) {
+			translate(p->children[i]);
+			i++;
+		}
+		interpret(p);
+	}
 }
